a977c44e1379c62f117b50fe500ae1388d88f8b0
Gagandeep Singh <gagandeep.singh.sde@gmail.com>
2024-04-07 01:56:09 -0400

    chacha20 done

--- main.c
+++ main.c
@@ -70,24 +70,16 @@ void add_states(uint32_t final_state[16], const uint32_t original_state[16])
   }
 }
 
-void serialized_block(uint32_t *array, uint8_t *serialized_array)
+void serialized_block(uint32_t *array, uint8_t *key_stream)
 {
   for (int i = 0; i < 16; i++)
   {
-    serialized_array[i * 4 + 0] = (array[i] >> 0) & 0xFF;
-    serialized_array[i * 4 + 1] = (array[i] >> 8) & 0xFF;
-    serialized_array[i * 4 + 2] = (array[i] >> 16) & 0xFF;
-    serialized_array[i * 4 + 3] = (array[i] >> 24) & 0xFF;
-
-    printf("%02x ", (array[i] >> 0) & 0xFF);
-    printf("%02x ", (array[i] >> 8) & 0xFF);
-    printf("%02x ", (array[i] >> 16) & 0xFF);
-    printf("%02x ", (array[i] >> 24) & 0xFF);
-    if ((i + 1) % 4 == 0)
-    {
-      printf("\n");
-    }
+    key_stream[i * 4 + 0] = (array[i] >> 0) & 0xFF;
+    key_stream[i * 4 + 1] = (array[i] >> 8) & 0xFF;
+    key_stream[i * 4 + 2] = (array[i] >> 16) & 0xFF;
+    key_stream[i * 4 + 3] = (array[i] >> 24) & 0xFF;
   }
+  printf("\n");
 }
 
 void chacha20_block(const uint8_t key[32], const uint8_t nonce[12], uint32_t counter, uint8_t *key_stream)
@@ -99,6 +91,15 @@ void chacha20_block(const uint8_t key[32], const uint8_t nonce[12], uint32_t cou
   inner_block(state);
   add_states(state, original_state);
   serialized_block(state, key_stream);
+  for (int i = 0; i < 16; i++)
+  {
+    printf("%02x ", state[i]);
+    if ((i + 1) % 4 == 0)
+    {
+      printf("\n");
+    }
+  }
+  printf("\n");
 }
 
 void xor_blocks(const uint8_t block1[64], const uint8_t block2[64], uint8_t result[64])
@@ -135,19 +136,18 @@ int main()
 {
   uint8_t key[32] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
                      0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f};
-  uint8_t nonce[12] = {0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x00};
+  uint8_t nonce[12] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x00};
   uint32_t counter = 1;
   uint8_t plaintext[] = "Ladies and Gentlemen of the class of '99: If I could offer you only one tip for the future, sunscreen would be it.";
 
   // Encryption
-  size_t plaintext_len = strlen((char *)plaintext);
-  size_t encrypted_len = (plaintext_len + BLOCK_SIZE - 1) / BLOCK_SIZE * BLOCK_SIZE;
-  uint8_t encrypted_message[encrypted_len];
-  chacha20_encrypt(key, nonce, counter, plaintext, plaintext_len, encrypted_message);
-
+  size_t msg_len = strlen((char *)plaintext);
+  uint8_t encrypted_message[msg_len];
+  memset(encrypted_message, 0, msg_len);
+  chacha20_encrypt(key, nonce, counter, plaintext, msg_len, encrypted_message);
+  printf("Encrypted message");
   printf("\n");
-  printf("\n");
-  for (int i = 0; i < encrypted_len; i++)
+  for (int i = 0; i < msg_len; i++)
   {
     printf("%02x ", encrypted_message[i]);
     if ((i + 1) % 16 == 0)
@@ -155,6 +155,5 @@ int main()
       printf("\n");
     }
   }
-
   return 0;
 }
